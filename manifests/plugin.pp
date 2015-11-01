71d934cc6312778a26b3f97e90928902e1175187
Eugene Dementiev <eugene@dementiev.eu>
2015-11-01 10:57:19 +0300

    Add support for toml arrays

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,11 +1,16 @@
 define telegraf::plugin (
-  $order = '10',
-  $conf  = undef,
+  $order      = '10',
+  $conf       = undef,
+  $plugin_name  = undef,
 ) {
   include telegraf::params
 
+  if $plugin_name {
+    $real_name = $plugin_name
+  } else {
+    $real_name = $title
+  }
   $plugin_file = regsubst($title, '\[|\]', '', 'G')
-  $plugin_name = $title
 
   concat::fragment { $plugin_file:
     target  => $telegraf::params::conf_path,
