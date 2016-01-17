a9eb4a1d71c37c7cff1af4161cfc0306be2dbd9c
Eugene Dementiev <eugene@dementiev.eu>
2016-01-17 16:46:25 +0300

    add amqp output influxdb params

--- manifests/outputs.pp
+++ manifests/outputs.pp
@@ -1,21 +1,27 @@
 class telegraf::outputs::amqp (
-  $url         = undef,
-  $exchange    = '',
-  $routing_tag = '',
-  $ssl_ca      = '',
-  $ssl_cert    = '',
-  $ssl_key     = '',
+  $url              = undef,
+  $exchange         = '',
+  $routing_tag      = '',
+  $ssl_ca           = '',
+  $ssl_cert         = '',
+  $ssl_key          = '',
+  $precision        = 'n',
+  $retention_policy = 'default',
+  $database         = 'telegraf',
 ) {
 
   telegraf::plugin { '[outputs.amqp]':
     order => '03',
     conf  => {
-      'url'         => $url,
-      'exchange'    => $exchange,
-      'routing_tag' => $routing_tag,
-      'ssl_ca'      => $ssl_ca,
-      'ssl_cert'    => $ssl_cert,
-      'ssl_key'     => $ssl_key,
+      'url'              => $url,
+      'exchange'         => $exchange,
+      'routing_tag'      => $routing_tag,
+      'ssl_ca'           => $ssl_ca,
+      'ssl_cert'         => $ssl_cert,
+      'ssl_key'          => $ssl_key,
+      'precision'        => $precision,
+      'retention_policy' => $retention_policy,
+      'database'         => $database,
     }
   }
 }
