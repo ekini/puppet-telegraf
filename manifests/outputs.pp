743538e9a5d19ea85e02d996ea5aa28c278400f1
Eugene Dementiev <eugene@dementiev.eu>
2016-01-17 05:37:46 +0300

    add ssl options for amqp

--- manifests/outputs.pp
+++ manifests/outputs.pp
@@ -2,6 +2,9 @@ class telegraf::outputs::amqp (
   $url         = undef,
   $exchange    = '',
   $routing_tag = '',
+  $ssl_ca      = '',
+  $ssl_cert    = '',
+  $ssl_key     = '',
 ) {
 
   telegraf::plugin { '[outputs.amqp]':
@@ -10,6 +13,9 @@ class telegraf::outputs::amqp (
       'url'         => $url,
       'exchange'    => $exchange,
       'routing_tag' => $routing_tag,
+      'ssl_ca'      => $ssl_ca,
+      'ssl_cert'    => $ssl_cert,
+      'ssl_key'     => $ssl_key,
     }
   }
 }
